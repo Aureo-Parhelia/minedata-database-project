783566742b3e4d78d33058bbb6dec3df72f756ba
Aureo-Parhelia <laureanoorejhair@gmail.com>
2026-07-06 21:07:28 -0500

    Update metallurgical_process.js

--- database/non_relational/metallurgical_process.js
+++ database/non_relational/metallurgical_process.js
@@ -1,4 +1,4 @@
-use MINEDATA_NOTSQL;
+use("MINEDATA_SOLUTION"); 
 
 db.createCollection("metallurgical_process", {
     "capped": false,
@@ -7,50 +7,82 @@ db.createCollection("metallurgical_process", {
             "bsonType": "object",
             "title": "metallurgical_process",
             "properties": {
-                "_id": { "bsonType": "objectId" },
-                "id_process": { "bsonType": "int" },
-                "start_date_time": { "bsonType": "date" },
-                "end_date_time": { "bsonType": "date" },
+                "_id": {
+                    "bsonType": "objectId"
+                },
+                "start_date_time": {
+                    "bsonType": "date"
+                },
+                "end_date_time": {
+                    "bsonType": "date"
+                },
                 "worker": {
                     "bsonType": "object",
                     "properties": {
-                        "id_worker": { "bsonType": "int" },
-                        "first_name": { "bsonType": "string" },
-                        "last_name": { "bsonType": "string" }
+                        "id_worker": {
+                            "bsonType": "int"
+                        },
+                        "first_name": {
+                            "bsonType": "string"
+                        },
+                        "last_name": {
+                            "bsonType": "string"
+                        }
                     },
-                    "additionalProperties": false,
-                    "required": ["id_worker", "first_name", "last_name"]
+                    "additionalProperties": false
                 },
-                "operation_equipment": {
+                "operation_equipament": {
                     "bsonType": "object",
                     "properties": {
-                        "id_equipment": { "bsonType": "int" },
-                        "equipment_code": { "bsonType": "string" },
-                        "equipment_name": { "bsonType": "string" }
-                     },
-                    "additionalProperties": false,
-                    "required": ["id_equipment", "equipment_code", "equipment_name"]
+                        "id_equipment": {
+                            "bsonType": "int"
+                        },
+                        "equipment_code": {
+                            "bsonType": "string"
+                        },
+                        "equipment_name": {
+                            "bsonType": "string"
+                        }
+                    },
+                    "additionalProperties": false
                 },
                 "process_measurement": {
                     "bsonType": "array",
-                    "items": {
-                        "bsonType": "object",
-                        "properties": {
-                            "id_measurement": { "bsonType": "int" },
-                            "recorded_value": { "bsonType": "double" },
-                            "measurement_time": { "bsonType": "date" },
-                            "is_out_range": { "bsonType": "bool" }
+                    "additionalItems": true,
+                    "items": [
+                        {
+                            "bsonType": "int"
                         },
-                        "additionalProperties": false,
-                        "required": ["id_measurement", "recorded_value", "measurement_time", "is_out_range"]
-                    }
+                        {
+                            "bsonType": "double"
+                        },
+                        {
+                            "bsonType": "date"
+                        },
+                        {
+                            "bsonType": "binData"
+                        }
+                    ]
                 },
-                "id_batch": { "bsonType": "int" }
+                "process_parameter": {
+                    "bsonType": "object",
+                    "properties": {
+                        "parameter_name": {
+                            "bsonType": "string"
+                        },
+                        "unit_of_measure": {
+                            "bsonType": "string"
+                        }
+                    },
+                    "additionalProperties": false
+                },
+                "id_batch": {
+                    "bsonType": "int"
+                }
             },
-            "additionalProperties": false,
-            "required": ["id_process", "start_date_time", "end_date_time", "worker", "operation_equipment", "process_measurement", "id_batch"]
+            "additionalProperties": false
         }
     },
-    "validationLevel": "strict",
-    "validationAction": "error"
+    "validationLevel": "off",
+    "validationAction": "warn"
 });
