ab64f5b25c95d3638d98adc8652e8708d8fc9941
Aureo-Parhelia <laureanoorejhair@gmail.com>
2026-07-08 21:26:18 -0500

    Update schema.js

--- database/non_relational/schema.js
+++ database/non_relational/schema.js
@@ -1,7 +1,8 @@
-// ============================================================
-// SCRIPT DE VALIDACIÓN: ENFOQUE NO RELACIONAL (MONGODB)
-// ============================================================
-use("MINEDATA_SOLUTION"); 
+/*
+ ============================================================
+    SCRIPT DE CREACIÖN MODELO NO RELACIONAL (MongoDB)
+ ============================================================
+*/
 
 db.createCollection("mineral_batch", {
     "capped": false,
@@ -17,47 +18,65 @@ db.createCollection("mineral_batch", {
                     "bsonType": "string"
                 },
                 "batch_weight_tons": {
-                    "bsonType": "double"
+                    "bsonType": "decimal"
                 },
                 "entry_status": {
                     "bsonType": "string"
                 },
-                "ganga": {
-                    "bsonType": "object",
-                    "properties": {
-                        "id_ganga": {
-                            "bsonType": "string"
-                        },
-                        "description": {
-                            "bsonType": "string"
-                        }
-                    },
-                    "additionalProperties": false
-                },
                 "mineral_reception": {
                     "bsonType": "object",
                     "properties": {
-                        "id_reception": {
-                            "bsonType": "int"
-                        },
                         "reception_date": {
-                            "bsonType": "string"
+                            "bsonType": "date"
+                        },
+                        "description": {
+                            "bsonType": "string",
+                            "maxLength": 300
+                        },
+                        "ganga": {
+                            "bsonType": "object",
+                            "properties": {
+                                "entry_date": {
+                                    "bsonType": "date"
+                                },
+                                "description": {
+                                    "bsonType": "string"
+                                },
+                                "origin_zone": {
+                                    "bsonType": "string"
+                                },
+                                "total-wigth_tons": {
+                                    "bsonType": "decimal"
+                                }
+                            },
+                            "additionalProperties": false,
+                            "required": [
+                                "entry_date",
+                                "description",
+                                "origin_zone",
+                                "total-wigth_tons"
+                            ]
                         }
                     },
-                    "additionalProperties": false
+                    "additionalProperties": false,
+                    "required": [
+                        "reception_date",
+                        "description",
+                        "ganga"
+                    ]
                 }
             },
             "additionalProperties": false,
             "required": [
+                "batch_code",
                 "batch_weight_tons",
                 "entry_status",
-                "ganga",
                 "mineral_reception"
             ]
         }
     },
-    "validationLevel": "off",
-    "validationAction": "warn"
+    "validationLevel": "strict",
+    "validationAction": "error"
 });
 
 
@@ -79,75 +98,123 @@ db.createCollection("metallurgical_process", {
                 "end_date_time": {
                     "bsonType": "date"
                 },
+                "id_batch": {
+                    "bsonType": "objectId"
+                },
                 "worker": {
                     "bsonType": "object",
                     "properties": {
-                        "id_worker": {
-                            "bsonType": "int"
-                        },
                         "first_name": {
                             "bsonType": "string"
                         },
                         "last_name": {
                             "bsonType": "string"
+                        },
+                        "email": {
+                            "bsonType": "string",
+                            "maxLength": 100
+                        },
+                        "position": {
+                            "bsonType": "object",
+                            "properties": {
+                                "position_name": {
+                                    "bsonType": "string"
+                                }
+                            },
+                            "additionalProperties": false,
+                            "required": [
+                                "position_name"
+                            ]
                         }
                     },
-                    "additionalProperties": false
+                    "additionalProperties": false,
+                    "required": [
+                        "first_name",
+                        "last_name",
+                        "email",
+                        "position"
+                    ]
                 },
-                "operation_equipament": {
+                "work_shift": {
                     "bsonType": "object",
                     "properties": {
-                        "id_equipment": {
-                            "bsonType": "int"
+                        "shift_name": {
+                            "bsonType": "string"
                         },
+                        "start_time": {
+                            "bsonType": "date"
+                        },
+                        "end_time": {
+                            "bsonType": "date"
+                        }
+                    },
+                    "additionalProperties": false,
+                    "required": [
+                        "shift_name",
+                        "start_time",
+                        "end_time"
+                    ]
+                },
+                "operation_equipment": {
+                    "bsonType": "object",
+                    "properties": {
                         "equipment_code": {
                             "bsonType": "string"
                         },
                         "equipment_name": {
                             "bsonType": "string"
+                        },
+                        "status": {
+                            "bsonType": "string",
+                            "maxLength": 30
                         }
                     },
-                    "additionalProperties": false
-                },
-                "process_measurement": {
-                    "bsonType": "array",
-                    "additionalItems": true,
-                    "items": [
-                        {
-                            "bsonType": "int"
-                        },
-                        {
-                            "bsonType": "double"
-                        },
-                        {
-                            "bsonType": "date"
-                        },
-                        {
-                            "bsonType": "binData"
-                        }
+                    "additionalProperties": false,
+                    "required": [
+                        "equipment_code",
+                        "equipment_name",
+                        "status"
                     ]
                 },
-                "process_parameter": {
+                "concentrate": {
                     "bsonType": "object",
                     "properties": {
-                        "parameter_name": {
-                            "bsonType": "string"
+                        "purity_percentage": {
+                            "bsonType": "decimal"
                         },
-                        "unit_of_measure": {
-                            "bsonType": "string"
+                        "weight_tons": {
+                            "bsonType": "decimal"
                         }
                     },
                     "additionalProperties": false
                 },
-                "id_batch": {
-                    "bsonType": "int"
+                "tailings": {
+                    "bsonType": "object",
+                    "properties": {
+                        "toxicity_level": {
+                            "bsonType": "string",
+                            "maxLength": 100
+                        },
+                        "weight_tons": {
+                            "bsonType": "decimal"
+                        }
+                    },
+                    "additionalProperties": false
                 }
             },
-            "additionalProperties": false
+            "additionalProperties": false,
+            "required": [
+                "start_date_time",
+                "end_date_time",
+                "id_batch",
+                "worker",
+                "work_shift",
+                "operation_equipment"
+            ]
         }
     },
-    "validationLevel": "off",
-    "validationAction": "warn"
+    "validationLevel": "strict",
+    "validationAction": "error"
 });
 
 
@@ -166,31 +233,71 @@ db.createCollection("laboratory_analysis", {
                 "analysis_code": {
                     "bsonType": "string"
                 },
+                "mineral_grade_porcentage": {
+                    "bsonType": "decimal"
+                },
                 "analysis_date": {
                     "bsonType": "date"
                 },
-                "batch_code": {
+                "status": {
                     "bsonType": "string"
+                },
+                "id_batch": {
+                    "bsonType": "objectId"
+                },
+                "mineral_sample": {
+                    "bsonType": "object",
+                    "properties": {
+                        "sample_code": {
+                            "bsonType": "string"
+                        },
+                        "extraction_date": {
+                            "bsonType": "date"
+                        }
+                    },
+                    "additionalProperties": false,
+                    "required": [
+                        "sample_code",
+                        "extraction_date"
+                    ]
                 },
                 "sieve_analysis": {
                     "bsonType": "array",
-                    "additionalItems": true,
-                    "items": [
-                        {
-                            "bsonType": "string"
+                    "additionalItems": false,
+                    "items": {
+                        "bsonType": "object",
+                        "properties": {
+                            "mesh_number": {
+                                "bsonType": "string"
+                            },
+                            "weight_retained": {
+                                "bsonType": "decimal"
+                            },
+                            "percentage_passing": {
+                                "bsonType": "decimal"
+                            }
                         },
-                        {
-                            "bsonType": "double"
-                        },
-                        {
-                            "bsonType": "string"
-                        }
-                    ]
+                        "additionalProperties": false,
+                        "required": [
+                            "mesh_number",
+                            "weight_retained",
+                            "percentage_passing"
+                        ]
+                    }
                 }
             },
-            "additionalProperties": false
+            "additionalProperties": false,
+            "required": [
+                "analysis_code",
+                "mineral_grade_porcentage",
+                "analysis_date",
+                "status",
+                "id_batch",
+                "mineral_sample",
+                "sieve_analysis"
+            ]
         }
     },
-    "validationLevel": "off",
-    "validationAction": "warn"
+    "validationLevel": "strict",
+    "validationAction": "error"
 });
